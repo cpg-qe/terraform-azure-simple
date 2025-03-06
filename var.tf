f28c5811a486cb20324fc2286c965d98c4129a6f
cpg-qe <swathi.g@servicenow.com>
2025-03-06 17:06:07 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -142,12 +142,3 @@ variable "objectVar" {
  })
  description = "Tags for the EC2 instance"
 }
-
-variable "TagDiskVar" {
-  type = map
-  default = {
-    cpg = "Disk"
-    Team  = "CPG"
-    // Add more tags as needed
-  }
-}

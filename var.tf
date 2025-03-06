3d614987055b11b0707c50c1312b4c572f5bd0ec
cpg-qe <swathi.g@servicenow.com>
2025-03-06 18:13:11 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -3,7 +3,10 @@ variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
 }
 
-variable "region" {}
+variable "region" {
+  description = "The Azure Region in which all resources in this example should be created"
+  default = "westus"
+}
 
 
 variable "subscriptionId" {}
